ddf6425e535f7288381ea881e09b8e55f9062431
Daniel Macuare <daniel280187@hotmail.com>
2026-02-06 21:10:04 +0100

    Enabling DNS Hostnames for public hosts

--- modules/create-vpc/vpc.tf
+++ modules/create-vpc/vpc.tf
@@ -1,6 +1,7 @@
 resource "aws_vpc" "this" {
   cidr_block                       = var.vpc_cidr
   assign_generated_ipv6_cidr_block = var.assign_generated_ipv6_cidr_block
+  enable_dns_hostnames = true
 
   tags = merge(
     var.default_tags,
