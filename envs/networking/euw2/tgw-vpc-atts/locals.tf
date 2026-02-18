68bd166e84652936f7c8b2013a38f4fc46536a0e
Daniel Macuare <daniel280187@hotmail.com>
2026-02-18 17:34:19 +0100

    feat: Dynamic Data Sources for Remote State

--- envs/networking/euw2/tgw-vpc-atts/locals.tf
+++ envs/networking/euw2/tgw-vpc-atts/locals.tf
@@ -2,6 +2,32 @@ locals {
   region       = "eu-west-2"
   region_short = "euw2"
 
+  # Dynamic cell mapping for different environments
+  # Only state_key is needed - environment and vpc_name come from remote state
+  cell_mappings = {
+    # Development environment cells
+    dev = {
+      cell1000 = {
+        state_key = "env-dev/euw2/cell1000/terraform.tfstate"
+      }
+      # cell0001 = {
+      #   state_key = "env-dev/euw2/cell0001/terraform.tfstate"
+      # }  # TODO: Uncomment when cell0001 VPC is created
+    }
+    # Production environment cells
+    prod = {
+      cell1000 = {
+        state_key = "env-prod/euw2/cell1000/terraform.tfstate"
+      }
+      cell0001 = {
+        state_key = "env-prod/euw2/cell0001/terraform.tfstate"
+      }
+    }
+  }
+
+  # Current environment from variable
+  current_environment = var.target_environment
+
   default_tags = {
     owning_team          = "NETENG"
     managed_by_terraform = true
