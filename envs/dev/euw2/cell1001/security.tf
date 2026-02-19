045bf1c478fae80e426af04368686f224860b606
Daniel Macuare <daniel280187@hotmail.com>
2026-02-20 00:21:33 +0100

    feat: Add cell1001 euw2 - Part 2

---
+++ envs/dev/euw2/cell1001/security.tf
@@ -0,0 +1,14 @@
+module "security" {
+  source       = "../../../../modules/security"
+  vpc_id       = module.vpc-main.vpc.id
+  vpc_cidr     = module.vpc-main.vpc.cidr_block # From VPC module output
+  region_short = local.region_short
+  environment  = local.environment
+  default_tags = local.default_tags
+
+  public_subnet_ids  = [for k, v in module.vpc-main.public_subnets : v.id]
+  private_subnet_ids = [for k, v in module.vpc-main.private_subnets : v.id]
+
+  # Allow all dev euw2 cells (10.1.0.0/16) to communicate via TGW
+  env_supernet_cidr = "10.1.0.0/16"
+}
