86a4d9fdd463116c8abf07310548ca4cb449f9d8
Daniel Macuare <daniel280187@hotmail.com>
2026-02-24 11:49:10 +0100

    feat: Keypair folder for EUW2 Dev

---
+++ envs/dev/euw2/keypair/backend.tf
@@ -0,0 +1,10 @@
+# A backend block cannot refer to named values (like input variables, locals, or data source attributes).
+terraform {
+  backend "s3" {
+    region       = "eu-west-2"
+    bucket       = "dmac-bootstrap-tfstate"
+    key          = "env-dev/euw2/keypair/terraform.tfstate"
+    encrypt      = true
+    use_lockfile = true
+  }
+}
