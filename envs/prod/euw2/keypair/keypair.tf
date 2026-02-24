99cb1e9c444b9bb44b3e00938c9748fd1b79c16a
Daniel Macuare <daniel280187@hotmail.com>
2026-02-24 11:48:55 +0100

    feat: Keypair folder for EUW2 Prod

---
+++ envs/prod/euw2/keypair/keypair.tf
@@ -0,0 +1,7 @@
+module "key_pair" {
+  source       = "../../../../modules/create-key-pair"
+  project_root = local.project_root
+  region_short = local.region_short
+  environment  = local.environment
+  default_tags = local.default_tags
+}
