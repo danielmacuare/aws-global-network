e40fddf13870884232da6933f499dbdd02ea798b
Daniel Macuare <daniel280187@hotmail.com>
2026-02-25 11:43:45 +0100

    feat: Add euw1 prod cell2000

---
+++ envs/prod/euw1/cell2000/locals.tf
@@ -0,0 +1,35 @@
+locals {
+  region       = "eu-west-1"
+  region_short = "euw1"
+  environment  = "prod"
+  cell_name    = "cell2000"
+  vpc_name     = "${local.region_short}-${local.environment}-${local.cell_name}"
+  project_root = pathexpand("~/repos/aws-global-network")
+
+  default_tags = {
+    owning_team          = "NETENG"
+    managed_by_terraform = true
+    environment          = local.environment
+    region               = local.region
+    region_short         = local.region_short
+    cell_name            = local.cell_name
+  }
+
+  private_subnets = {
+    priv-0 = {
+      az          = "${local.region}a"
+      cidr        = "10.16.0.0/24"
+      nat_gateway = true
+    }
+    priv-1 = {
+      az          = "${local.region}b"
+      cidr        = "10.16.1.0/24"
+      nat_gateway = true
+    }
+    priv-2 = {
+      az          = "${local.region}c"
+      cidr        = "10.16.2.0/24"
+      nat_gateway = true
+    }
+  }
+}
