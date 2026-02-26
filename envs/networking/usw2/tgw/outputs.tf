68561ec9aee800924f2eba9debe59e7ca4f93db8
Daniel Macuare <daniel280187@hotmail.com>
2026-02-26 23:10:18 +0100

    feat:  Add USW2 TGW

---
+++ envs/networking/usw2/tgw/outputs.tf
@@ -0,0 +1,24 @@
+output "transit_gateway" {
+  description = "Transit Gateway resource"
+  value       = module.tgw.transit_gateway
+}
+
+output "route_table_prod" {
+  description = "Production route table"
+  value       = module.tgw.route_table_prod
+}
+
+output "route_table_dev" {
+  description = "Development route table"
+  value       = module.tgw.route_table_dev
+}
+
+output "route_table_shared" {
+  description = "Shared services route table"
+  value       = module.tgw.route_table_shared
+}
+
+output "route_table_wan" {
+  description = "WAN route table for TGW peering attachments"
+  value       = module.tgw.route_table_wan
+}
