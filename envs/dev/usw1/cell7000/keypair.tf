4df3ba85ec421f57575d0c079bde1053f0cf61ef
Daniel Macuare <daniel280187@hotmail.com>
2026-02-27 00:16:38 +0100

    fix: Remove USW1 as it only has 2 AZs

--- envs/dev/usw1/cell7000/keypair.tf
+++
@@ -1,3 +0,0 @@
-data "aws_key_pair" "this" {
-  key_name = "kp-${local.region_short}-${local.environment}"
-}
--- envs/networking/usw1/tgw/tgw.tf
+++
@@ -1,8 +0,0 @@
-module "tgw" {
-  source = "../../../../modules/create-tgw"
-
-  region          = local.region
-  region_short    = local.region_short
-  amazon_side_asn = local.amazon_side_asn
-  default_tags    = local.default_tags
-}
