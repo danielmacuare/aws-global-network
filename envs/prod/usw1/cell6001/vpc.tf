821dd4970c3ef9f1036c1df81c4f0f9ae61a67c8
Daniel Macuare <daniel280187@hotmail.com>
2026-02-26 23:13:40 +0100

    feat:  Add USW1 Prod Cell6001

---
+++ envs/prod/usw1/cell6001/vpc.tf
@@ -0,0 +1,39 @@
+module "vpc-main" {
+  source = "../../../../modules/create-vpc/"
+
+  region       = local.region
+  region_short = local.region_short
+  environment  = local.environment
+  vpc_name     = local.vpc_name
+  cell_name    = local.cell_name
+  vpc_cidr     = "10.48.16.0/20"
+  default_tags = local.default_tags
+
+  private_subnets = {
+    priv-0 = {
+      az          = "${local.region}a"
+      cidr        = "10.48.16.0/24"
+      nat_gateway = true
+    }
+    priv-1 = {
+      az          = "${local.region}b"
+      cidr        = "10.48.17.0/24"
+      nat_gateway = true
+    }
+  }
+
+  public_subnets = {
+    pub-0 = {
+      az          = "${local.region}a"
+      cidr        = "10.48.26.0/24"
+      nat_gateway = false
+    }
+    pub-1 = {
+      az          = "${local.region}b"
+      cidr        = "10.48.27.0/24"
+      nat_gateway = false
+    }
+  }
+
+  assign_generated_ipv6_cidr_block = "true"
+}
