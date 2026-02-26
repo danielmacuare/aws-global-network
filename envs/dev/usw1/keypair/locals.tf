41c255d0e83e324551dbd31a8eccfbf3a4f476e7
Daniel Macuare <daniel280187@hotmail.com>
2026-02-26 23:01:58 +0100

    feat:  Add USW1 Dev SSH Key Pairs

---
+++ envs/dev/usw1/keypair/locals.tf
@@ -0,0 +1,14 @@
+locals {
+  region       = "us-west-1"
+  region_short = "usw1"
+  environment  = "dev"
+  project_root = pathexpand("~/repos/aws-global-network")
+
+  default_tags = {
+    owning_team          = "NETENG"
+    managed_by_terraform = true
+    environment          = local.environment
+    region               = local.region
+    region_short         = local.region_short
+  }
+}
