04e2e97cedf1ecfa71374099631ce7777903eaa0
Daniel Macuare <daniel280187@hotmail.com>
2026-02-27 01:00:46 +0100

    feat:  Add USE1 SSH Key Pairs

---
+++ envs/dev/use1/keypair/locals.tf
@@ -0,0 +1,14 @@
+locals {
+  region       = "us-east-1"
+  region_short = "use1"
+  environment  = "dev"
+  project_root = pathexpand("~/repos/aws-global-network")
+
+  default_tags = {
+    owning_team          = "NETENG"
+    managed_by_terraform = true
+    environment          = local.environment
+    region               = local.region
+    region_short         = local.region_short
+  }
+}
